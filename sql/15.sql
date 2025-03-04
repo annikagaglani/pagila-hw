d04323e8ec4bae51b7b42173e223f093ffcde794
annikagaglani <agaglan@students.pitzer.edu>
2025-03-04 15:22:55 -0800

    added problem 15

--- sql/15.sql
+++ sql/15.sql
@@ -2,3 +2,11 @@
  * Use a JOIN to count the number of English language films in each category.
  * Use table category, film_category, film, and language.
  */
+SELECT c.name, count(f.film_id) AS sum
+FROM category c
+JOIN film_category USING (category_id)
+JOIN film f USING (film_id)
+JOIN language l  USING (language_id)
+WHERE l.name = 'English'
+GROUP BY c.name
+ORDER BY c.name;
